4bd06b28d703f3b2b25ff458e06baaba970caa2a
Ahmed Al Salih <a.alsalih2@gmail.com>
2024-12-17 22:59:23 -0500

    disply the uploaded image in the frontend
    Signed-off-by: Ahmed Al Salih <a.alsalih2@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
 
-//const App = () => {
-function App() {
+
+
+const App = () => {
+  //function App() {
+  const [image, setImage] = useState(null)
+
+  const uploadImage = (e) => {
+    //console.log(e.target.files)
+    const formData = new FormData()
+    formData.append('fike', e.target.files[0])
+    setImage(e.target.files[0])
+    e.target.value = null
+    
+  }
+
   return (
-
-    <div>
-
+    <div className="app">
+      {image && <img src={URL.createObjectURL(image)} />}
+      <label htmlFor="files">upload an image</label>
+      <input onChange={uploadImage} id="files" accept="image/*" type="file" />
     </div>
   );
 }
